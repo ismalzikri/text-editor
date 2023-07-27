6cca94ed2868be1b3bf425b1107ebc830fedea3e
Laravel007 <zikriismal42@gmail.com>
2023-07-27 17:01:57 +0700

    update-home-pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,63 @@
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>rendering text editor</p>
+    <main className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
+        <div className="flex h-20 justify-between items-center">
+          <div>
+            <h1 className="capitalize">
+              <b>Somethings</b>
+            </h1>
+          </div>
+          <nav>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "secondary", size: "sm" }),
+                "px-4"
+              )}
+            >
+              Login
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <Link
+            href={siteConfig.links.twitter}
+            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+            target="_blank"
+          >
+            Follow along on Twitter
+          </Link>
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            An example text editor built using Next.js 13 server components.
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            I&apos;m building a web app with Next.js 13 and open sourcing
+            everything. Follow along as we figure this out together.
+          </p>
+          <div className="space-x-4">
+            <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+              Get Started
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "../types";
+
+export const siteConfig: SiteConfig = {
+  name: "Custom editor",
+  description:
+    "An example customs text editor using server components in Next.js 13.",
+  url: "https://custom-text-editor.netlify.app/",
+  ogImage: "https://custom-text-editor.netlify.app/bear-with-keyboard.png",
+  links: {
+    twitter: "https://twitter.com/ZikriIsmal",
+    github: "https://github.com/ismalzikri",
+  },
+};
