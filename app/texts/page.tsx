c45e4e08dc6f6b073d8fe021f5a43fda837710f8
Laravel007 <zikriismal42@gmail.com>
2023-08-05 21:21:23 +0700

    update submit with server action

--- app/texts/page.tsx
+++ app/texts/page.tsx
@@ -1,10 +1,12 @@
 import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
 
 export const metadata = {
   title: "Texts",
 };
 
 export default async function TextsPage() {
+  const user = await getCurrentUser();
   const texts = await db.text.findMany({
     select: {
       id: true,
@@ -15,9 +17,30 @@ export default async function TextsPage() {
     orderBy: { updatedAt: "desc" },
   });
 
+  const handleSubmit = async (data: FormData) => {
+    "use server";
+    const post = await db.text.create({
+      data: {
+        title: data.get("title")?.toString() || "",
+        content: data.get("content")?.toString() || "",
+        // authorId: user.id,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(post);
+  };
+
   return (
     <main>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
       <pre>{JSON.stringify(texts, null, 2)}</pre>
+      <form id="submit-form" action={handleSubmit}>
+        <input type="text" name="title" className="border" required />
+        <textarea name="content" required></textarea>
+        <button type="submit">save text</button>
+      </form>
     </main>
   );
 }
